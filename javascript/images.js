231c8773e0a7146426e7f644611047c4702c13cf
Matthew Kaiser <mjkaiser10@gmail.com>
2016-06-21 14:05:29 -0400

    Create images.js

---
+++ javascript/images.js
@@ -0,0 +1,7 @@
+// init Masonry after all images have loaded
+var $grid = $('.grid').imagesLoaded( function() {
+  $grid.masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+  }); 
+});
